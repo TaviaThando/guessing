1dfc48d75a9d76613765f84c3cd1d68dbc319ee5
octmoyo021 <octmoyo021@student.wethinkcode.co.za>
2022-01-10 20:07:10 +0200

    figured out how to get user input ...

--- number_guess.java
+++ number_guess.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+//import java.util.Random;
+
 public class number_guess {
     public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("We are going to play a number guessing game");
+
+        System.out.println("Guess a number from 1 to 50");
+
+        int num_guess = number.nextInt();     
+
         
+
     }
 }
